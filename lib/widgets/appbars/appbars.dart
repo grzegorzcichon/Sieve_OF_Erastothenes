5347257552884fad11553d1cffd9317950085eaa
Grzegorz Cichioń <grzegorz@cichon.dev>
2023-02-08 19:16:13 +0100

    first commit

---
+++ lib/widgets/appbars/appbars.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:sieveoferatosthenes/resources/colors/colors.dart';
+import 'package:sieveoferatosthenes/resources/strings/strings.dart';
+import 'package:sieveoferatosthenes/widgets/text_styles.dart';
+
+import '../text_styles.dart';
+
+class CustomAppbar extends StatelessWidget with PreferredSizeWidget {
+  @override
+  Widget build(BuildContext context) {
+    return AppBar(
+      backgroundColor: Theme.of(context).colorScheme.AppbarColor,
+      title: Center(
+          child: Text(Str.label.sieveoferatosthenes,
+              style: AppabarTextStyles.overline(
+                  color: Theme.of(context).colorScheme.FontsColor,
+                  context: context))),
+      systemOverlayStyle: SystemUiOverlayStyle.dark,
+      shadowColor: Color.fromARGB(255, 246, 218, 127),
+      // shape: RoundedRectangleBorder(
+      // borderRadius: BorderRadius.all(Radius.circular(30))),
+    );
+  }
+
+  @override
+  Size get preferredSize => Size.fromHeight(kToolbarHeight);
+}
